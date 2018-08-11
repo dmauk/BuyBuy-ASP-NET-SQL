472a4c4e655e9f1f734ba40afe39aa3f64d4e1e2
Dan Mauk <dmaukdev@gmail.com>
2018-08-11 13:44:02 -0400

    Initial Commit

---
+++ BuyBuy/Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using BuyBuy.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace BuyBuy.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer> {
+                new Customer { Name = "Bob Bob", Id = 2 },
+                new Customer { Name = "Richard Richar", Id = 1}
+            };
+        }
+    }
+}
---
+++ BuyBuy/Controllers/MoviesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BuyBuy.Models;
+using BuyBuy.ViewModels;
+
+namespace BuyBuy.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        // GET: Movies/ByReleaseDate
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Name = "Shrek!", Id = 1},
+                new Movie { Name = "Mama Mia", Id = 2}
+            };
+        }
+
+    }
+}
