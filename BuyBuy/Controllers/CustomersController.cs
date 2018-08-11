ded541ba374478d7f9b6f0f829974619872a7031
Dan Mauk <dmaukdev@gmail.com>
2018-08-11 15:01:45 -0400

    Updating customer definition to include membership type

--- BuyBuy/Controllers/CustomersController.cs
+++ BuyBuy/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using BuyBuy.Models;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Web.Mvc;
 using System.Linq;
@@ -7,30 +8,33 @@ namespace BuyBuy.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList(); //immediately query the database
             return View(customers);
         }
 
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer> {
-                new Customer { Name = "Bob Bob", Id = 2 },
-                new Customer { Name = "Richard Richar", Id = 1}
-            };
         }
     }
 }
--- BuyBuy/Models/Customer.cs
+++ BuyBuy/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,11 @@ namespace BuyBuy.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public String Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeID { get; set; }
     }
 }
