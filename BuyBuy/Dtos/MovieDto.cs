2d69ac75fc26efe80a0eff2d1b863a5fe6e9496d
Dan Mauk <dmaukdev@gmail.com>
2018-08-13 00:42:40 -0400

    Adding REST API

---
+++ BuyBuy/Dtos/MovieDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BuyBuy.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public String Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Range(1, 999)]
+        [Required]
+        public int NumberInStock { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+    }
+}
